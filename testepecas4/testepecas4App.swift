207d3fdd0767a39b857e5958102a86b1510cbb84
taissa-rodrigues <taissavrp@gmail.com>
2023-10-16 14:46:08 -0300

    Initial Commit

---
+++ testepecas4/testepecas4App.swift
@@ -0,0 +1,17 @@
+//
+//  testepecas4App.swift
+//  testepecas4
+//
+//  Created by user on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct testepecas4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
